1f54f0c61ce6c8bd6b7418918f60563f6f6442db
shefattonmoy <shefathossain7@gmail.com>
2024-01-05 23:02:38 +0600

    checkout done

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,8 +1,74 @@
-import React from 'react';
+import React from "react";
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
+import "../styles/Checkout.css";
+import { useSelector } from "react-redux";
 
 const Checkout = () => {
+
+  const totalQuantity = useSelector(state => state.cart.totalQuantity);
+  const totalAmount = useSelector(state => state.cart.totalAmount);
+
   return (
-    <div>Checkout</div>
+    <Helmet title="Checkout">
+      <CommonSection title="Checkout"></CommonSection>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="8">
+              <h6 className="mb-4 fw-bold">Billing Information</h6>
+              <Form className="billing-form">
+                <FormGroup className="form-group">
+                  <input type="text" placeholder="Your Name" />
+                </FormGroup>
+                <FormGroup className="form-group">
+                  <input type="email" placeholder="Your Email" />
+                </FormGroup>
+                <FormGroup className="form-group">
+                  <input type="number" placeholder="Your Contact No." />
+                </FormGroup>
+                <FormGroup className="form-group">
+                  <input type="text" placeholder="Street Address" />
+                </FormGroup>
+                <FormGroup className="form-group">
+                  <input type="text" placeholder="City" />
+                </FormGroup>
+                <FormGroup className="form-group">
+                  <input type="number" placeholder="Postal Code" />
+                </FormGroup>
+                <FormGroup className="form-group">
+                  <input type="text" placeholder="Country" />
+                </FormGroup>
+              </Form>
+            </Col>
+            <Col lg="4">
+              <div className="checkout-cart">
+                <h6>
+                  Total Quantity: <span>{totalQuantity} item(s)</span>
+                </h6>
+                <h6>
+                  Subtotal: <span>$100</span>
+                </h6>
+                <h6>
+                  <span>
+                    Shipping: <br />
+                    Free Shipping
+                  </span>
+                  <span>$0</span>
+                </h6>
+                <h4>
+                  Total Cost: <span>${totalAmount}</span>
+                </h4>
+                <button className="checkout-btn auth-btn w-100">Place Order</button>
+              </div>
+              
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   );
 };
 
